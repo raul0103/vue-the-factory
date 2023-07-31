af32f1b128fa876a33774282df62201f91d840f0
raul0103 <rshakurov95@mail.ru>
2023-08-01 00:20:18 +0600

    Исправил добавление избранного

--- src/composable/favorite.js
+++ src/composable/favorite.js
@@ -1,10 +1,16 @@
+/**
+ * favorites- необходимо полулчать при каждом обращении
+ * Поэтому он находится в каждой функции
+ */
+
 import { pushData, getData, popData } from "@/services/storage.js";
 
 export function useFavorite() {
   const storage_key = "favorites";
-  const favorites = getData(storage_key);
 
   function setFavorite(data) {
+    const favorites = getData(storage_key);
+
     const find_index = favorites
       ? favorites.findIndex((item) => item.id == data.id)
       : -1;
@@ -17,6 +23,8 @@ export function useFavorite() {
   }
 
   function checkFavoriteById(id) {
+    const favorites = getData(storage_key);
+
     const favorite_ids = favorites?.map((item) => item.id);
 
     if (favorite_ids.indexOf(id) > -1) {
@@ -27,6 +35,8 @@ export function useFavorite() {
   }
 
   function getFavorites() {
+    const favorites = getData(storage_key);
+
     return favorites;
   }
 
